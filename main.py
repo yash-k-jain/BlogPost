07fecbe991bd64c2917aa1b30808472b992d9568
yash-k-jain <107643838+yash-k-jain@users.noreply.github.com>
2023-10-05 12:23:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -289,10 +289,10 @@ def edit_blog():
 def delete():
     form = DeleteConfirm()
     if form.validate_on_submit():
-          id = request.args.get("blog_id")
-          required_post = db.get_or_404(BlogPost, id)
-          db.session.delete(required_post)
-          db.session.commit()
+      id = request.args.get("blog_id")
+      required_post = db.get_or_404(BlogPost, id)
+      db.session.delete(required_post)
+      db.session.commit()
           return redirect(url_for("show_all_user_blogs", name=current_user.name))
     return render_template("confirm_delete.html", form=form)
 
